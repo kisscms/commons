796b5a0219c76683cc38a311250b2b2f918d334a
Makis Tracend <makis.tracend@gmail.com>
2013-06-25 22:48:30 -0700

    adding config options

---
+++ bin/config.php
@@ -0,0 +1,16 @@
+<?php
+
+
+//===============================================
+// Configuration
+//===============================================
+
+if( class_exists('Config') && method_exists(new Config(),'register')){ 
+
+	// Register variables
+	Config::register("commons", "css", "http://cdn.kdi.co/css/common/0.7.0/common.min.css");
+	Config::register("commons", "js", "http://cdn.kdi.co/js/common/0.3.0/common-min.js");
+
+}
+
+?>
