8e27d80b3f3350ca0fd56b6c4fa8c51d568c5dfe
Makis Tracend <makis.tracend@gmail.com>
2013-06-26 17:49:31 -0700

    adding data-path attribute on common.js

--- views/foot.php
+++ views/foot.php
@@ -1,1 +1,1 @@
-<script type="text/javascript" src="<?=$GLOBALS['config']['commons']['js']?>" data-type="require"></script>
+<script type="text/javascript" src="<?=$GLOBALS['config']['commons']['js']?>" data-path="common" data-type="require"></script>
