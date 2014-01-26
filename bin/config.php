c831f7fff2bbe0e5b2a1723808de64f63de850d9
Makis Tracend <makis.tracend@gmail.com>
2014-01-26 01:45:38 -0800

    bumping common.js version to 0.4.0

--- bin/config.php
+++ bin/config.php
@@ -9,7 +9,7 @@ if( class_exists('Config') && method_exists(new Config(),'register')){
 
 	// Register variables
 	Config::register("commons", "css", "http://cdn.kdi.co/css/common/0.7.0/common.min.css");
-	Config::register("commons", "js", "http://cdn.kdi.co/js/common/0.3.0/common-min.js");
+	Config::register("commons", "js", "http://cdn.kdi.co/js/common/0.4.0/common-min.js");
 
 }
 
